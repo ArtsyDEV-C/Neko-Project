5a707f4780e8f1acba6dd78e874a57a95339b99f
Shreejith Kumar Das S <135417968+ArtsyDEV-C@users.noreply.github.com>
2025-03-16 12:30:49 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -7,17 +7,17 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+    if (!this.isModified('password')) return next();
+    try {
+        this.password = await bcrypt.hash(this.password, 12);  // 🔄 Optimized Hashing
+        next();
+    } catch (error) {
+        next(error);
+    }
 });
 
 UserSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
-
-
-
 
 module.exports = mongoose.model("User", UserSchema);
