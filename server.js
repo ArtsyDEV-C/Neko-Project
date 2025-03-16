f3dd41dc0cdd6f43582fdf781f6714d2a5d8bebe
Shreejith Kumar Das S <135417968+ArtsyDEV-C@users.noreply.github.com>
2025-03-16 05:48:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(express.json({ limit: "10mb" })); // Increase limit if needed
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors());
 app.use(methodOverride('_method'));
-const path = require("path");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 
