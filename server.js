4f97aa6ca478bf2c64aef3f8fa038854a47c2115
Shreejith Kumar Das S <135417968+ArtsyDEV-C@users.noreply.github.com>
2025-03-16 07:41:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,8 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors());
 app.use(methodOverride('_method'));
 
-app.use(express.static(path.join(__dirname, "public")));
+const path = require("path");
+app.use(express.static(path.resolve(__dirname, "public")));
 
 
 // Express session
